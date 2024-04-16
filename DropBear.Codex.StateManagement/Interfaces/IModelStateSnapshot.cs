7b9700e050a44583794bb0311dc3c81b9383a43a
DropBear <tkuchel@users.noreply.github.com>
2024-04-16 12:08:56 +1000

    Version Bump. AppLogger fixes.

--- DropBear.Codex.StateManagement/Interfaces/IModelStateSnapshot.cs
+++ DropBear.Codex.StateManagement/Interfaces/IModelStateSnapshot.cs
@@ -1,10 +1,34 @@
-﻿using ServiceStack.Text;
+﻿using System.Text.Json;
 
 namespace DropBear.Codex.StateManagement.Interfaces;
 
 public interface IModelStateSnapshot
 {
-    void InitializeSnapshot<T>(T model, TimeSpan expiration, bool trackChanges = false, JsConfigScope? options = null);
-    bool IsModelDirty<T>(T model, out IEnumerable<string> changedProperties, JsConfigScope? options = null);
-    void ClearSnapshot<T>(T model, JsConfigScope? options = null);
+    /// <summary>
+    /// Initializes monitoring of a model by taking a snapshot of its state and storing it in the cache with a sliding expiration policy.
+    /// </summary>
+    /// <typeparam name="T">The type of the model to monitor.</typeparam>
+    /// <param name="model">The model to monitor.</param>
+    /// <param name="expiration">The amount of time before the snapshot expires and is removed from the cache.</param>
+    /// <param name="trackChanges">Should changes to the entity be tracked.</param>
+    /// <param name="options">Optional custom JsonSerializerOptions for serialization settings.</param>
+    void InitializeSnapshot<T>(T model, TimeSpan expiration, bool trackChanges = false, JsonSerializerOptions? options = null);
+
+    /// <summary>
+    /// Checks if the model has changed since its snapshot was taken.
+    /// </summary>
+    /// <typeparam name="T">The type of the model to check.</typeparam>
+    /// <param name="model">The current state of the model.</param>
+    /// <param name="changedProperties">An enumerable of string representing the changed properties.</param>
+    /// <param name="options">Optional custom JsonSerializerOptions for serialization settings.</param>
+    /// <returns>True if the model has changed; otherwise, false.</returns>
+    bool HasModelChanged<T>(T model, out IEnumerable<string> changedProperties, JsonSerializerOptions? options = null);
+
+    /// <summary>
+    /// Clears a model's snapshot from the cache.
+    /// </summary>
+    /// <typeparam name="T">The type of the model.</typeparam>
+    /// <param name="model">The model whose snapshot should be cleared.</param>
+    /// <param name="options">Optional custom JsonSerializerOptions for serialization settings.</param>
+    void ClearSnapshot<T>(T model, JsonSerializerOptions? options = null);
 }
--- DropBear.Codex.StateManagement/ModelState/ModelStateSnapshot.cs
+++ DropBear.Codex.StateManagement/ModelState/ModelStateSnapshot.cs
@@ -1,49 +1,57 @@
 ﻿using System.Collections;
+using System.Collections.Concurrent;
 using System.Reflection;
 using System.Runtime.Caching;
 using System.Text;
+using System.Text.Json;
 using Blake2Fast;
 using Cysharp.Text;
-using DropBear.Codex.AppLogger.Interfaces;
+using DropBear.Codex.AppLogger.Builders;
 using DropBear.Codex.StateManagement.Extensions;
 using DropBear.Codex.StateManagement.Interfaces;
-using ServiceStack.Text;
+using Microsoft.Extensions.Logging;
+using ILoggerFactory = DropBear.Codex.AppLogger.Interfaces.ILoggerFactory;
 
 namespace DropBear.Codex.StateManagement.ModelState;
 
-public class ModelStateSnapshot(IAppLogger<ModelStateSnapshot> logger) : IModelStateSnapshot
+public class ModelStateSnapshot : IModelStateSnapshot
 {
-    private readonly Dictionary<Type, PropertyInfo[]> _propertiesCache = new();
+    private readonly ILogger<ModelStateSnapshot> _logger;
+    private readonly ConcurrentDictionary<Type, PropertyInfo[]> _propertiesCache = new();
     private readonly MemoryCache _snapshotCache = MemoryCache.Default;
+  
+    public ModelStateSnapshot() => _logger = LoggerFactory.CreateLogger<ModelStateSnapshot>();
+
+    private static ILoggerFactory LoggerFactory => new LoggerConfigurationBuilder()
+        .SetLogLevel(LogLevel.Information)
+        .EnableConsoleOutput()
+        .Build();
+
 
     /// <summary>
-    ///     Initializes monitoring of a model by taking a snapshot of its state and storing it in the cache with an expiration
-    ///     policy.
+    ///     Initializes monitoring of a model by taking a snapshot of its state and storing it in the cache with a sliding
+    ///     expiration policy.
     /// </summary>
     /// <typeparam name="T">The type of the model to monitor.</typeparam>
     /// <param name="model">The model to monitor.</param>
     /// <param name="expiration">The amount of time before the snapshot expires and is removed from the cache.</param>
     /// <param name="trackChanges">Should changes to the entity be tracked.</param>
-    /// <param name="options">Optional custom JsConfig scope for serialization settings.</param>
+    /// <param name="options">Optional custom JsonSerializerOptions for serialization settings.</param>
     public void InitializeSnapshot<T>(T model, TimeSpan expiration, bool trackChanges = false,
-        JsConfigScope? options = null)
+        JsonSerializerOptions? options = null)
     {
         try
         {
-            using (options)
-            {
-                var serializedModel = JsonSerializer.SerializeToString(model);
-                var cacheKey = GenerateCacheKey(serializedModel, typeof(T));
-                var dataToStore = trackChanges ? serializedModel : ComputeHash(serializedModel);
+            var serializedModel = JsonSerializer.Serialize(model, options);
+            var cacheKey = GenerateCacheKey(serializedModel, typeof(T));
+            var dataToStore = trackChanges ? serializedModel : ComputeHash(serializedModel);
 
-                var cacheItemPolicy =
-                    new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.UtcNow.Add(expiration) };
-                _snapshotCache.Set(new CacheItem(cacheKey, dataToStore), cacheItemPolicy);
-            }
+            var cacheItemPolicy = new CacheItemPolicy { SlidingExpiration = expiration };
+            _snapshotCache.Set(new CacheItem(cacheKey, dataToStore), cacheItemPolicy);
         }
         catch (Exception ex)
         {
-            logger.LogError(ex, ZString.Format("Error initializing snapshot: {0}", ex.Message));
+            _logger.LogError(ex, ZString.Format("Error initializing snapshot: {0}", ex.Message));
         }
     }
 
@@ -53,62 +61,55 @@ public class ModelStateSnapshot(IAppLogger<ModelStateSnapshot> logger) : IModelS
     /// <typeparam name="T">The type of the model to check.</typeparam>
     /// <param name="model">The current state of the model.</param>
     /// <param name="changedProperties">An enumerable of string representing the changed properties.</param>
-    /// <param name="options">Optional custom JsConfig scope for serialization settings.</param>
-    /// <returns>True if the model is dirty; otherwise, false.</returns>
-    public bool IsModelDirty<T>(T model, out IEnumerable<string> changedProperties, JsConfigScope? options = null)
+    /// <param name="options">Optional custom JsonSerializerOptions for serialization settings.</param>
+    /// <returns>True if the model has changed; otherwise, false.</returns>
+    public bool HasModelChanged<T>(T model, out IEnumerable<string> changedProperties,
+        JsonSerializerOptions? options = null)
     {
-        changedProperties = new List<string>();
+        changedProperties = Enumerable.Empty<string>();
         try
         {
-            using (options)
-            {
-                var serializedModel = JsonSerializer.SerializeToString(model);
-                var cacheKey = GenerateCacheKey(serializedModel, typeof(T));
+            var serializedModel = JsonSerializer.Serialize(model, options);
+            var cacheKey = GenerateCacheKey(serializedModel, typeof(T));
 
-                if (_snapshotCache.Get(cacheKey) is not string storedData)
-                    return true; // Assume dirty if snapshot not found
+            if (_snapshotCache.Get(cacheKey) is not string storedData)
+                return true; // Assume changed if snapshot not found
 
-                // If storedData is a hash, then it's not tracking changes
-                if (storedData.Length == ComputeHash(serializedModel).Length)
-                    return !ComputeHash(serializedModel).Equals(storedData, StringComparison.Ordinal);
+            // If storedData is a hash, then it's not tracking changes
+            if (storedData.Length == ComputeHash(serializedModel).Length)
+                return !ComputeHash(serializedModel).Equals(storedData, StringComparison.Ordinal);
 
-                // Change tracking is enabled, compare serialized states
-                changedProperties = GetChangedProperties(serializedModel, storedData);
-                using var enumerator = changedProperties.GetEnumerator();
-                return enumerator.MoveNext(); // True if any changes
-            }
+            // Change tracking is enabled, compare serialized states
+            changedProperties = GetChangedProperties<T>(serializedModel, storedData);
+            return changedProperties.Any();
         }
         catch (Exception ex)
         {
-            logger.LogError(ex, ZString.Format("Error checking model dirty: {0}", ex.Message));
-            return true; // Assume dirty in case of error
+            _logger.LogError(ex, ZString.Format("Error checking model changes: {0}", ex.Message));
+            return true; // Assume changed in case of error
         }
     }
-
 
     /// <summary>
     ///     Clears a model's snapshot from the cache.
     /// </summary>
     /// <typeparam name="T">The type of the model.</typeparam>
     /// <param name="model">The model whose snapshot should be cleared.</param>
-    /// <param name="options">Optional custom JsConfig scope for serialization settings.</param>
-    public void ClearSnapshot<T>(T model, JsConfigScope? options = null)
+    /// <param name="options">Optional custom JsonSerializerOptions for serialization settings.</param>
+    public void ClearSnapshot<T>(T model, JsonSerializerOptions? options = null)
     {
         try
         {
-            using (options)
-            {
-                var serializedModel = JsonSerializer.SerializeToString(model);
-                var cacheKey = GenerateCacheKey(serializedModel, typeof(T));
-                _snapshotCache.Remove(cacheKey);
-            }
+            var serializedModel = JsonSerializer.Serialize(model, options);
+            var cacheKey = GenerateCacheKey(serializedModel, typeof(T));
+            _snapshotCache.Remove(cacheKey);
         }
         catch (Exception ex)
         {
-            // Log exception or handle it according to your application's error handling policy
-            logger.LogError(ex, ZString.Format("Error clearing snapshot: {0}", ex.Message));
+            _logger.LogError(ex, ZString.Format("Error clearing snapshot: {0}", ex.Message));
         }
     }
+
 
     /// <summary>
     ///     Retrieves changed properties between two model states.
@@ -117,27 +118,36 @@ public class ModelStateSnapshot(IAppLogger<ModelStateSnapshot> logger) : IModelS
     /// <param name="current">The current state of the model.</param>
     /// <param name="original">The original state of the model.</param>
     /// <returns>A distinct list of property names that have changed.</returns>
-    private IEnumerable<string> GetChangedProperties<T>(T? current, T? original) =>
-        CheckForChanges(current, original, string.Empty).Distinct(StringComparer.Ordinal);
+    private IEnumerable<string> GetChangedProperties<T>(string current, string original)
+    {
+        var currentObj = JsonSerializer.Deserialize<T>(current);
+        var originalObj = JsonSerializer.Deserialize<T>(original);
+
+        return CheckForChanges(currentObj, originalObj, string.Empty).Distinct(StringComparer.Ordinal);
+    }
 
     /// <summary>
     ///     Recursively checks for changes between the current and original model states.
     /// </summary>
     private IEnumerable<string> CheckForChanges(object? current, object? original, string basePath)
     {
-        if (current is null || original is null) yield break;
+        if (current is null || original is null)
+            yield break;
 
         var type = current.GetType();
-        if (type != original.GetType()) yield break;
+        if (type != original.GetType())
+            yield break;
+
         if (type.IsSimpleType() || type.IsEnum)
         {
-            if (!Equals(current, original)) yield return basePath.Trim('.');
+            if (!Equals(current, original))
+                yield return basePath.Trim('.');
         }
         else if (typeof(IEnumerable).IsAssignableFrom(type) && type != typeof(string))
         {
             // Handle collections with complex objects
-            List<object?> currentItems = ((IEnumerable)current).Cast<object>().ToList()!;
-            List<object?> originalItems = ((IEnumerable)original).Cast<object>().ToList()!;
+            var currentItems = ((IEnumerable)current).Cast<object>().ToList();
+            var originalItems = ((IEnumerable)original).Cast<object>().ToList();
 
             if (currentItems.Count != originalItems.Count)
                 yield return basePath;
@@ -157,7 +167,8 @@ public class ModelStateSnapshot(IAppLogger<ModelStateSnapshot> logger) : IModelS
                 var currentPropValue = property.GetValue(current);
                 var originalPropValue = property.GetValue(original);
                 foreach (var change in CheckForChanges(currentPropValue, originalPropValue,
-                             $"{basePath}.{property.Name}")) yield return change;
+                             $"{basePath}.{property.Name}"))
+                    yield return change;
             }
         }
     }
