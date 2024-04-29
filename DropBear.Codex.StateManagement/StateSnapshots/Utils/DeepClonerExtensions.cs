137323f160b26e40a582acaea7843788aef9cdab
DropBear <tkuchel@users.noreply.github.com>
2024-04-29 15:39:27 +1000

    Added extension method variations for DeepCloner.

---
+++ DropBear.Codex.StateManagement/StateSnapshots/Utils/DeepClonerExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using DropBear.Codex.Core;
+using Newtonsoft.Json;
+
+namespace DropBear.Codex.StateManagement.StateSnapshots.Utils;
+
+public static class DeepClonerExtensions
+{
+    public static Result<T> Clone<T>(this T obj, JsonSerializerSettings? settings = null) where T : class
+    {
+        try
+        {
+            return DeepCloner.Clone(obj, settings);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error cloning object: {ex.Message}");
+            return Result<T>.Failure("An error occurred while cloning the object.");
+        }
+    }
+
+    public static async Task<Result<T>> CloneAsync<T>(this T obj, JsonSerializerSettings? settings = null)
+        where T : class
+    {
+        try
+        {
+            return await Task.Run(() => DeepCloner.Clone(obj, settings)).ConfigureAwait(false);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error cloning object: {ex.Message}");
+            return Result<T>.Failure("An error occurred while cloning the object.");
+        }
+    }
+}
